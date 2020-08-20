2a722131aa19b52815e73581f50d8b532848e20e
dessarion <dessuas@gmial.com>
2020-08-20 18:08:13 +0300

    first commit

---
+++ src/components/CryptoList.vue
@@ -0,0 +1,121 @@
+<template>
+  <div>
+    <h1>Current cryptocyrrency listing</h1>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>
+            <input />
+            <span>O</span>
+          </th>
+          <th>
+            Price
+            <select v-model="quote">
+              <option v-for="quote in getQuotes" :value="quote" :key="quote">{{quote}}</option>
+            </select>
+          </th>
+          <th>Trading volume 24h</th>
+          <th>Market capitalization</th>
+        </tr>
+      </thead>
+      <Loader v-if="loadingStatus" />
+      <tbody v-else>
+        <tr v-for="(cryptoItem, idx) in findByText" :key="cryptoItem.id">
+          <td>{{idx + 1}}</td>
+          <td class="name">
+            <span class="name_span">{{cryptoItem.name}}</span>
+            <span class="name_span">{{cryptoItem.symbol}}</span>
+          </td>
+          <td>
+            <span  class="price">{{cryptoItem.quote[quote].price | floatComaDigit}}</span>
+            <span>( {{cryptoItem.quote[quote].percent_change_24h | floatComaPercent}} % )</span>
+          </td>
+          <td>{{cryptoItem.quote[quote].volume_24h.toFixed(0)}} {{quote}}'s</td>
+          <td>{{cryptoItem.quote[quote].market_cap.toFixed(0)}} {{quote}}'s</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions, mapMutations } from "vuex";
+export default {
+  data() {
+    return {
+      quote: "",      
+    };
+  },
+  computed: {
+    ...mapGetters(["getList", "loadingStatus", "getQuotes"]),
+    getFilteredList() {
+      if (this.quote === "") {
+        this.quote = this.getQuotes[0];
+        return this.getList[0];
+      }
+      const quotes = this.getQuotes;
+      const idx = quotes.indexOf(this.quote);
+      return this.getList[idx];
+    },
+    findByText(){      
+      return this.getFilteredList
+    },
+  },
+  methods: {
+    ...mapActions(["fetchCrypto"]),   
+  },
+  async mounted() {
+    this.fetchCrypto();
+  },
+  components: {
+    Loader: () => import("@/components/Loader"),
+  },
+  filters: {
+    floatComaDigit(digit) {
+      if(digit < 1) {
+        return digit.toFixed(3)
+      } else {
+        return digit.toFixed(2)
+      }
+    },
+    floatComaPercent(percent){
+      if(Math.abs(percent) < 1) {
+        return percent.toFixed(4)
+      } else {
+        return percent.toFixed(2)
+      }
+    },
+  }
+};
+</script>
+
+<style scoped>
+select {
+  padding: 5px;
+  margin-bottom: 20px;
+  outline: none;
+}
+table {
+  border: 1px solid black;
+  width: auto;
+  margin: 0 auto;
+}
+tbody td {
+  border: 1px solid black;
+  width: 200px;
+  height: 50px;
+}
+tbody td:first-child {
+  width: auto;
+}
+.price{
+  display: flex;
+  flex-direction: column;
+  font-weight: bold;
+}
+.name_span {
+  width: 100%;
+  display: inline-block;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    apiKey: process.env.VUE_APP_FIXER ,
+    quote: ['UAH', 'USD', 'EUR', 'NOK'],
+    cryptoList: [],
+    loading: true,
+  },
+  mutations: {
+    updateList(state, cryptoList) {
+      state.cryptoList.push(cryptoList)
+    },
+    updateLoadingStatus(state, status) {
+      state.loading = status
+    },
+  },
+  actions: {
+    async fetchCrypto({ commit }, limit = 4) {
+      let key = this.state.apiKey
+
+      const fetchIterator = async function (quotes) {
+        for (let i = 0; quotes.length > i; i++) {
+          let qouteStr = '?CMC_PRO_API_KEY=' + key + '&start=1&limit=' + limit + '&convert=' + quotes[i]
+          let response = await fetch('https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest' + qouteStr)
+          let cryptoList = await response.json()
+          commit("updateList", cryptoList.data)
+        }
+      }
+      fetchIterator(this.state.quote)
+
+      setTimeout(() => {
+        commit("updateLoadingStatus", false)
+      }, 1000);
+    },
+  },
+  getters: {
+    getList(state) {
+      return state.cryptoList
+    },
+    getQuote(state,) {
+      return state.quote[0]
+    },
+    getQuotes(state,) {
+      return state.quote
+    },
+    loadingStatus(state) {
+      return state.loading
+    },
+  },
+})
